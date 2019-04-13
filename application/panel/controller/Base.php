9173df43b826ad4ededd5ab3e620d23fb818a426
谢泽丰 <616973936@qq.com>
2019-04-13 13:33:14 +0800

    【feat】后台base控制器，控制侧栏及用户登录情况

---
+++ application/panel/controller/Base.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiezefeng
+ * Date: 2019/4/13
+ * Time: 1:20 PM
+ */
+
+namespace app\panel\controller;
+
+use think\App;
+use think\Controller;
+use think\facade\Session;
+
+class Base extends Controller
+{
+    // 图标来源 https://www.thinkcmf.com/font_awesome.html
+    public $menu = array(
+        array(
+            'c' => 'index',
+            'a' => 'index',
+            'title' => '控制台',
+            'icon' => 'dashboard',
+            'child' => array()
+        ),
+        array(
+            'c' => 'user',
+            'a' => 'index',
+            'title' => '用户管理',
+            'icon' => 'user',
+            'child' => array()
+        ),
+    );
+
+    function __construct(App $app = null)
+    {
+        parent::__construct($app);
+        if (!Session::has('panel_user')) {
+            $this->redirect('login/index');
+        }
+        $realname = empty(Session::get('panel_user.realname')) ? '匿名' : Session::get('panel_user.realname');
+        $this->assign('realname', $realname);
+        $this->assign('menu', $this->menu);
+    }
+
+}
