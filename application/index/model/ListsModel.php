2af9b2bcf14a086671cffbcef2bc31411523cd2a
谢泽丰 <616973936@qq.com>
2019-04-13 23:43:49 +0800

    【init】新增一些文件到新闻网站站点前端

---
+++ application/index/model/ListsModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiezefeng
+ * Date: 2019/4/13
+ * Time: 11:37 PM
+ */
+namespace app\index\model;
+
+use think\Model;
+
+class ListsModel extends Model
+{
+    protected $table = 'lists';
+}
---
+++ application/index/model/ViewsModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiezefeng
+ * Date: 2019/4/13
+ * Time: 11:37 PM
+ */
+namespace app\index\model;
+
+use think\Model;
+
+class ViewsModel extends Model
+{
+    protected $table = 'views';
+}
