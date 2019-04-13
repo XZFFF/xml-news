0822df7b764c8283a237aba814653870bad4398f
谢泽丰 <616973936@qq.com>
2019-04-13 19:43:53 +0800

    【fix】完成新增新闻功能，并完成测试

--- application/panel/controller/News.php
+++ application/panel/controller/News.php
@@ -9,6 +9,7 @@
 namespace app\panel\controller;
 
 use app\panel\model\ListsModel;
+use app\panel\model\NewsModel;
 use app\panel\validate\PanelValidate;
 use think\facade\Session;
 
@@ -27,5 +28,26 @@ class News extends Base
         $panel_user = Session::get('panel_user');
         $this->assign('panel_user', $panel_user);
         return $this->fetch();
+    }
+
+    public function add_news()
+    {
+        $req = input('post.');
+        $news_model = new NewsModel();
+        $panel_validate = new PanelValidate();
+        if ($panel_validate->scene('add_news')->check($req) != VALIDATE_PASS) {
+            return api_return(CODE_PARAM_ERROR, $panel_validate->getError());
+        }
+
+        $add_data = array(
+            'lists_id' => $req['lists_id'],
+            'title' => $req['news_title'],
+            'author' => $req['news_author'],
+            'is_show' => $req['news_is_show'],
+            'content' => $req['news_content'],
+            'publish_time' => date('Y:m:d H:i:s', time()),
+        );
+        $news_info = $news_model->add_news($add_data);
+        return api_return($news_info['code'], $news_info['msg'], $news_info['data']);
     }
 }
--- application/panel/model/NewsModel.php
+++ application/panel/model/NewsModel.php
@@ -13,4 +13,18 @@ use think\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
+
+    public function add_news($add_data)
+    {
+        try {
+            $ok = $this->strict(false)->insert($add_data);
+            if ($ok == false) {
+                return data_return(CODE_ERROR, '添加失败', $this->getError());
+            } else {
+                return data_return(CODE_SUCCESS, '添加成功', $ok);
+            }
+        } catch (\Exception $e) {
+            return data_return(CODE_ERROR, '数据库异常', $e->getMessage());
+        }
+    }
 }
