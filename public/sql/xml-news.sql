2234e0a4f5e5c5ab41688265b2fbf96682b35465
谢泽丰 <616973936@qq.com>
2019-04-13 23:12:57 +0800

    【sql】更新数据库结构，加入浏览表

--- public/sql/xml-news.sql
+++ public/sql/xml-news.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 2019-04-13 11:15:17
+-- Generation Time: 2019-04-13 17:12:31
 -- 服务器版本： 5.6.35
 -- PHP Version: 7.1.1
 
@@ -19,7 +19,7 @@ USE `xml_news`;
 -- --------------------------------------------------------
 
 --
--- 表的结构 `list`
+-- 表的结构 `lists`
 --
 
 DROP TABLE IF EXISTS `lists`;
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `lists` (
   `sort` int(11) NOT NULL COMMENT '栏目权重，从大到小排序',
   `is_show` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COMMENT='新闻栏目表';
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='新闻栏目表';
 
 -- --------------------------------------------------------
 
@@ -40,12 +40,25 @@ CREATE TABLE IF NOT EXISTS `lists` (
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '新闻id',
-  `list_id` int(11) NOT NULL COMMENT '所属栏目id',
+  `lists_id` int(11) NOT NULL COMMENT '所属栏目id',
   `title` varchar(255) NOT NULL COMMENT '标题',
   `author` varchar(64) NOT NULL COMMENT '作者',
   `content` text NOT NULL COMMENT '内容',
-  `view` int(11) NOT NULL COMMENT '浏览量',
   `publish_time` datetime NOT NULL COMMENT '发布时间，排序从近到远',
   `is_show` tinyint(1) NOT NULL COMMENT '是否发布',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COMMENT='新闻表';
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='新闻表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `views`
+--
+
+DROP TABLE IF EXISTS `views`;
+CREATE TABLE IF NOT EXISTS `views` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) NOT NULL,
+  `view_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='浏览表';
