0ab71015f0a265726fd894952c52309335117a7c
谢泽丰 <616973936@qq.com>
2019-04-14 12:00:36 +0800

    【style】调整所有的样式文件路径及对应的模板参数替代数据

--- config/template.php
+++ config/template.php
@@ -15,32 +15,38 @@
 
 return [
     // 模板引擎类型 支持 php think 支持扩展
-    'type'         => 'Think',
+    'type' => 'Think',
     // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写 3 保持操作方法
-    'auto_rule'    => 1,
+    'auto_rule' => 1,
     // 模板路径
-    'view_path'    => '',
+    'view_path' => '',
     // 模板后缀
-    'view_suffix'  => 'html',
+    'view_suffix' => 'html',
     // 模板文件名分隔符
-    'view_depr'    => DIRECTORY_SEPARATOR,
+    'view_depr' => DIRECTORY_SEPARATOR,
     // 模板引擎普通标签开始标记
-    'tpl_begin'    => '{',
+    'tpl_begin' => '{',
     // 模板引擎普通标签结束标记
-    'tpl_end'      => '}',
+    'tpl_end' => '}',
     // 标签库标签开始标记
     'taglib_begin' => '{',
     // 标签库标签结束标记
-    'taglib_end'   => '}',
+    'taglib_end' => '}',
     // 模板参数替换
-    'tpl_replace_string'       => [
-        '__CSS__'    => '/xml-news/public/static/css',
-        '__FONT__'   => '/xml-news/public/static/fonts',
-        '__JS__'     => '/xml-news/public/static/js',
-        '__IMG__'    => '/xml-news/public/static/images',
-        '__INDEX__'  => '/xml-news/public/index.php/index',
-        '__PANEL__'  => '/xml-news/public/index.php/panel',
-        '__UM__'     => '/xml-news/public/umeditor',
-        '__UE__'     => '/xml-news/public/ueditor',
+    'tpl_replace_string' => [
+        '__INDEX__' => '/xml-news/public/index.php/index',
+        '__INDEX_CSS__' => '/xml-news/public/static/index/css',
+        '__INDEX_FONT__' => '/xml-news/public/static/index/font',
+        '__INDEX_JS__' => '/xml-news/public/static/index/js',
+        '__INDEX_IMG__' => '/xml-news/public/static/index/images',
+
+        '__PANEL__' => '/xml-news/public/index.php/panel',
+        '__PANEL_CSS__' => '/xml-news/public/static/panel/css',
+        '__PANEL_FONT__' => '/xml-news/public/static/panel/fonts',
+        '__PANEL_JS__' => '/xml-news/public/static/panel/js',
+        '__PANEL_IMG__' => '/xml-news/public/static/panel/images',
+
+        '__UM__' => '/xml-news/public/umeditor',
+        '__UE__' => '/xml-news/public/ueditor',
     ]
 ];
