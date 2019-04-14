9fbe481bc423ec68947eae5b9b4f7490f0bbed29
谢泽丰 <616973936@qq.com>
2019-04-14 16:04:16 +0800

    【feat】完成新闻内容页面的渲染

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -63,10 +63,10 @@ class Index extends Controller
             $this->assign('lists_data', $lists_data);
 
             // 新闻列表
-            $news_data = $news_model->where(['is_show' => 1, 'lists_id' => $now_lists_id])->paginate(2,false,[
-                'query' => request()->param()
-            ]);
-//            dump($news_data);exit();
+            $news_data = $news_model->where(['is_show' => 1, 'lists_id' => $now_lists_id])
+                ->paginate(2, false, [
+                    'query' => request()->param()
+                ]);
             foreach ($news_data->items() as $key => $value) {
                 $lists_id = $value['lists_id'];
                 $news_data->items()[$key]['lists_name'] = $lists_model->field('name')->where(['id' => $lists_id])->find()['name'];
@@ -85,6 +85,29 @@ class Index extends Controller
 
     public function news()
     {
+        $now_news_id = input('get.news_id');
+        $lists_model = new ListsModel();
+        $news_model = new NewsModel();
+        $views_model = new ViewsModel();
+        try {
+            // 栏目列表
+            $lists_data = $lists_model->where(['is_show' => 0])->order('sort', 'desc')->select();
+            // 查询栏目下的新闻数
+            foreach ($lists_data as $key => $value) {
+                $lists_id = $value['id'];
+                $lists_data[$key]['news_num'] = count($news_model->where(['lists_id' => $lists_id, 'is_show' => 1])->select());
+            }
+            $this->assign('lists_data', $lists_data);
+
+            // 新闻详细信息
+            $news_data = $news_model->where(['id' => $now_news_id])->find();
+            $lists_id = $news_data['lists_id'];
+            $news_data['lists_name'] = $lists_model->field('name')->where(['id' => $lists_id])->find()['name'];
+            $news_data['views'] = $views_model->get_news_views($now_news_id)['data'];
+            $this->assign('news_data', $news_data);
+        } catch (\Exception $e) {
+            return data_return(CODE_ERROR, '数据获取失败', $e->getMessage());
+        }
         return $this->fetch();
     }
 
