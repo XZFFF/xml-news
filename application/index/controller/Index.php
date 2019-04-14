9a5cc830da4ada84f0ea0c73730f47aa1fe3ddd0
谢泽丰 <616973936@qq.com>
2019-04-14 16:10:27 +0800

    【style】删除没有调用的函数方法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -105,50 +105,13 @@ class Index extends Controller
             $news_data['lists_name'] = $lists_model->field('name')->where(['id' => $lists_id])->find()['name'];
             $news_data['views'] = $views_model->get_news_views($now_news_id)['data'];
             $this->assign('news_data', $news_data);
+
+            // 增加一次新闻的view
+            $views_model->set_news_views($now_news_id);
         } catch (\Exception $e) {
             return data_return(CODE_ERROR, '数据获取失败', $e->getMessage());
         }
         return $this->fetch();
     }
 
-    public function get_lists()
-    {
-        $lists_model = new ListsModel();
-        $news_model = new NewsModel();
-        $views_model = new ViewsModel();
-        try {
-            $lists_info = $lists_model->get_show_lists();
-            $resp['data'] = $lists_info['data'];
-            foreach ($resp['data'] as $key => $value) {
-                // 限制了只展示最新的10篇
-                $news_info = $news_model->get_show_lists_news($value['id']);
-                foreach ($news_info['data'] as $key2 => $value2) {
-                    $news_id = $value2['id'];
-                    $value2['view'] = $views_model->get_news_views($news_id)['data'];
-                }
-                $value['news'] = $news_info['data'];
-            }
-            return api_return(CODE_SUCCESS, '数据获取成功', $resp['data']);
-        } catch (\Exception $e) {
-            return data_return(CODE_ERROR, '数据获取失败', $e->getMessage());
-        }
-    }
-
-    public function get_news()
-    {
-        $news_id = input('post.news_id');
-        $news_model = new NewsModel();
-        $views_model = new ViewsModel();
-        $news_info = $news_model->get_show_news($news_id);
-        $news_info['data']['view'] = $views_model->get_news_views($news_id)['data'];
-        return api_return($news_info['code'], $news_info['msg'], $news_info['data']);
-    }
-
-    public function set_view()
-    {
-        $news_id = input('post.news_id');
-        $views_model = new ViewsModel();
-        $views_info = $views_model->set_news_views($news_id);
-        return api_return($views_info['code'], $views_info['msg'], $views_info['data']);
-    }
 }
--- application/index/model/NewsModel.php
+++ application/index/model/NewsModel.php
@@ -13,35 +13,4 @@ use think\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
-
-    public function get_show_lists_news($lists_id)
-    {
-        try {
-            $news = $this->where(['lists_id' => $lists_id, 'is_show' => 1])
-                ->limit(0, 10)
-                ->order('publish_time', 'desc')
-                ->select();
-            if ($news == false) {
-                return data_return(CODE_ERROR, '获取失败', $this->getError());
-            } else {
-                return data_return(CODE_SUCCESS, '获取成功', $news);
-            }
-        } catch (\Exception $e) {
-            return data_return(CODE_ERROR, '数据库异常', $e->getMessage());
-        }
-    }
-
-    public function get_show_news($news_id)
-    {
-        try {
-            $news = $this->where(['id' => $news_id, 'is_show' => 1])->find();
-            if ($news == false) {
-                return data_return(CODE_ERROR, '获取失败', $this->getError());
-            } else {
-                return data_return(CODE_SUCCESS, '获取成功', $news);
-            }
-        } catch (\Exception $e) {
-            return data_return(CODE_ERROR, '数据库异常', $e->getMessage());
-        }
-    }
 }
